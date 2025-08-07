825ed30ebce7dde951f01596ba1f69a4b3de2c19
str0ngb4d <dev@strongbad.net>
2025-08-06 23:35:14 -0400

    Delete DynamicGameStats project

--- DynamicGameStats/Patches/EntityBuffsRemoveCustomVarPatch.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DynamicGameStats.Patches {
-  //[HarmonyPatch(typeof(EntityBuffs), nameof(EntityBuffs.RemoveCustomVar))]
-  public class EntityBuffsRemoveCustomVarPatch {
-    private static void Postfix(EntityBuffs __instance, string _name) {
-      DynamicGameStats.OnCVarChange(__instance.parent, _name);
-    }
-  }
-}
--- DynamicGameStats/Patches/EntityBuffsSetCustomVarPatch.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DynamicGameStats.Patches {
-  //[HarmonyPatch(typeof(EntityBuffs), nameof(EntityBuffs.SetCustomVar))]
-  public class EntityBuffsSetCustomVarPatch {
-    private static void Postfix(EntityBuffs __instance, string _name) {
-      DynamicGameStats.OnCVarChange(__instance.parent, _name);
-    }
-  }
-}
